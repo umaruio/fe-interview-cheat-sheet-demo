fae5f298761b43ad40e8e05445a3bdaf8878f835
Lawrence Lee <lawrence.lee@foxmail.com>
2017-07-26 17:37:10 +0800

    bubble sort

---
+++ Algorithm/Sort/Bubble Sort.js
@@ -0,0 +1,11 @@
+function sort (arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = tmp
+            }
+        }
+    }
+}
