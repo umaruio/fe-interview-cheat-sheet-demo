7b7a2ee471dd0a6f47421db4932e5904ca60b4be
Lawrence Lee <lawrence.lee@foxmail.com>
2017-07-27 13:32:41 +0800

    ajax

---
+++ JavaScript/Ajax/Ajax.js
@@ -0,0 +1,21 @@
+function createAjax () {
+    if (window.ActiveXObject) {
+        return new ActiveXObject("Microsoft.XMLHTTP")
+    } else {
+        return new XMLHttpRequest()
+    }
+}
+
+var xhr = createAjax()
+xhr.open('GET', 'http://jsonplaceholder.typicode.com/posts')
+xhr.onreadystatechange = function () {
+    console.log(xhr.readyState)
+    if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            document.getElementById('container').innerHTML = xhr.responseText
+        } else {
+            console.error(xhr.responseText)
+        }
+    }
+}
+xhr.send()
